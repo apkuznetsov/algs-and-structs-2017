5f63cf74d7218f78efd27e75fd4cc1bd59f150de
Andrey <ethenaut@gmail.com>
2022-08-14 16:40:47 +0400

    counting sorting

---
+++ yandex/tasks/src/main/java/kuznetsov/CountingSorting.java
@@ -0,0 +1,29 @@
+package kuznetsov;
+
+import java.util.Arrays;
+
+public class CountingSorting {
+
+    public void sort(int[] nums) {
+        int minVal = Arrays.stream(nums).min().getAsInt();
+        int maxVal = Arrays.stream(nums).max().getAsInt();
+        int uniqueCnt = maxVal - minVal + 1;
+        int[] minusedRepetitNums = new int[uniqueCnt];
+
+        for (int num : nums) {
+            int minusedCurrVal = num - minVal;
+            int oldValCnt = minusedRepetitNums[minusedCurrVal];
+            minusedRepetitNums[minusedCurrVal] = oldValCnt + 1;
+        }
+
+        int currNumsIdx = 0;
+        for (int i = 0; i < minusedRepetitNums.length; i++) {
+            int valCnt = minusedRepetitNums[i];
+            int currVal = i + minVal;
+            for (int j = 0; j < valCnt; j++) {
+                nums[currNumsIdx] = currVal;
+                currNumsIdx++;
+            }
+        }
+    }
+}
