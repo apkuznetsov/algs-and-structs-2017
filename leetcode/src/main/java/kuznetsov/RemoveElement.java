9b00813f3a2b5f708114d3fa973c4b5566e70a21
Andrey <ethenaut@gmail.com>
2022-10-07 10:11:51 +0400

    remove element

---
+++ leetcode/src/main/java/kuznetsov/RemoveElement.java
@@ -0,0 +1,19 @@
+package kuznetsov;
+
+// Given an integer array nums and an integer val, remove all occurrences of val in nums in-place.
+// The relative order of the elements may be changed.
+public class RemoveElement {
+
+    public int removeElement(int[] nums, int val) {
+        int wrIdx = 0;
+
+        for (int rdIdx = 0; rdIdx < nums.length; rdIdx++) {
+            if (nums[rdIdx] != val) {
+                nums[wrIdx] = nums[rdIdx];
+                wrIdx++;
+            }
+        }
+
+        return wrIdx;
+    }
+}
